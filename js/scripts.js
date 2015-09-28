75e432e663b00f48800f04a30472888a95eea4d3
Allison Tarr <hello@allisontarr.com>
2015-09-28 17:21:45 -0400

    sticky nav

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,42 @@ $(function(){
 	// Good ol' Smooth Scroll
 	$('a').smoothScroll();
 
-	// $('#menu-item-334').addClass('home-icon');
 });
+
+$('.mobile-icon').on('click', function(){
+	// $('.siteTitle').slideToggle(500);
+	$('#menu-left').slideToggle(500);
+	$('#menu-right').slideToggle(500);
+
+});
+
+
+//Sticky Nav
+$(document).ready(function() {
+  if ($("body").hasClass("home")){
+
+    //Calculate height
+    var totalHeight =$('.hero').outerHeight();
+
+    $(window).scroll(function() {
+      //if scrolled down more than the height of hero
+      if ($(window).scrollTop() > totalHeight) {
+        $('nav').addClass('sticky');
+        $('.main').css({"margin-top":"70px"});
+        } else {
+        // resetting nav when scrolled up
+        $('nav').removeClass('sticky');
+        $('.main').css({"margin-top":"0px"});
+
+      }; //end else statement
+    }); //end window scroll function  
+
+  } else {
+
+    $('nav').addClass('sticky');
+    $('body').css({
+        "margin-top":"70px"
+    });
+
+  } //end else statement
+}); //end function
