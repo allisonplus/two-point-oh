8f7835048ea6989b7674fb3d1e53c8c300a83b5c
Allison Tarr <allison@webdevstudios.com>
2015-11-07 18:03:29 -0500

    no about photo

--- front-page.php
+++ front-page.php
@@ -4,14 +4,14 @@
 <div class="main">
 
     <section class="welcome" id="about">
-	    <h2>Welcome!</h2>
+	    <!-- <h2>Welcome!</h2> -->
 	    <div class="container short-about">
 	      <div class="intro"><?php the_field('autobiographical_snippet', '12'); ?></div>
 	    	<?php $bio = get_field('autobiographical_photo', '12'); ?>
 	      
-	      <div class="welcome-photo-container">
+	      <!-- <div class="welcome-photo-container">
 		      <img class="welcome-photo" src="<?php echo $bio['sizes']['portfolio-front']; ?> " alt="Portrait of Allison smiling in front of brightly coloured graffiti wall">
-	      </div> <!--/.welcome-photo-container-->
+	      </div>  --><!--/.welcome-photo-container-->
 	    	
 			</div> <!--/.container-->
     </section> <!--/.welcome-->
@@ -65,7 +65,7 @@
     <div class="container">
     	<h2>Contact</h2>
 	    <div class="contact-info">
-	    	<p>I am currently accepting projects. If you have cool stuff to share or want to talk shop over a coffee, I'd love to hear from you!</p>
+	    	<p>If you have cool stuff to share or want to talk shop over a coffee, I'd love to hear from you!</p>
 	      <a class="emailMe" href="mailto:hello@allisontarr.com">hello@allisontarr.com</a>
 	    </div> <!--/.contact-info-->
 
