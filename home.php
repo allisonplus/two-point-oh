dd31d544cd5eea02f3cd534a57300b26946c32b1
Allison Tarr <allison@webdevstudios.com>
2016-02-15 19:06:11 -0500

    more tidy o' code

--- home.php
+++ home.php
@@ -10,27 +10,27 @@
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
         <div class="post">
-        	<?php if (  (function_exists('has_post_thumbnail')) && (has_post_thumbnail())  ) {
-        	  echo get_the_post_thumbnail($post->ID);
-        	} else {
-        	   echo main_image();
-        	} ?>
+          <?php if (  (function_exists('has_post_thumbnail')) && (has_post_thumbnail())  ) {
+            echo get_the_post_thumbnail($post->ID);
+          } else {
+             echo main_image();
+          } ?>
 
           <div class="blog-post-text">
             <h2><?php the_title(); ?></h2>
-            
+
             <div class="meta-data">
-    	        <p class="date"><?php echo get_the_date(); ?> in </p>
+              <p class="date"><?php echo get_the_date(); ?> in </p>
 
-    	        <?php 
-    	        $category = get_the_category(); 
+              <?php
+               $category = get_the_category();
 
-    	        if($category[0]){
-    	        echo '<a class="category" href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>';
-    	        } ?>
-    	        </div> <!--/.meta-data-->
+              if($category[0]){
+              echo '<a class="category" href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>';
+              } ?>
+              </div> <!--/.meta-data-->
 
-            <?php the_excerpt(); ?> 
+            <?php the_excerpt(); ?>
           </div> <!--/blog-post-text-->
         </div> <!--/.post-->
 
